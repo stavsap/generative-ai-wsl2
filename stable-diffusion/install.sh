0b683cbd1ec3fd8fe315c4b758fb2d0d89f6a80b
Stav Sapir <stavsap@gmail.com>
2023-05-10 20:30:38 +0300

    Update install.sh

--- stable-diffusion/install.sh
+++ stable-diffusion/install.sh
@@ -1,3 +1,6 @@
+curl https://raw.githubusercontent.com/stavsap/generative-ai-wsl2/main/stable-diffusion/run.sh > run.sh
+chmod +x run.sh
+
 git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git
 cd stable-diffusion-webui
 ./webui.sh
