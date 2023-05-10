14910168b7e675389872c3fed76a1345d5d0b956
Stav Sapir <stavsap@gmail.com>
2023-05-10 21:52:06 +0300

    Create install_full.sh

---
+++ dreambooth/install_full.sh
@@ -0,0 +1,14 @@
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt install software-properties-common -y
+sudo add-apt-repository ppa:deadsnakes/ppa -y
+sudo apt install python3.10 python3.10-venv python3-pip git python3.10-tk ffmpeg libsm6 libxext6 libgl1 wget -y
+sudo apt install nvidia-cuda-toolkit -y
+
+git clone https://github.com/bmaltais/kohya_ss
+cd kohya_ss
+./setup.sh -v
+source venv/bin/activate
+pip install bitsandbytes-cuda117
+pip install xformers
+./gui.sh --share --username admin --password password
