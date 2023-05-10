93df412a92907582a1265fd0a6ef2b61028f6810
Stav Sapir <stavsap@gmail.com>
2023-05-10 21:18:19 +0300

    Create install_full.sh

---
+++ Tortoise-TTS-Fast/install_full.sh
@@ -0,0 +1,15 @@
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt install software-properties-common -y
+sudo add-apt-repository ppa:deadsnakes/ppa -y
+sudo apt install python3.10 python3.10-venv python3-pip git python3.10-tk ffmpeg libsm6 libxext6 libgl1 wget -y
+sudo apt install nvidia-cuda-toolkit -y
+
+git clone https://github.com/thisserand/tortoise-tts-fast.git
+cd tortoise-tts-fast
+python3 -m venv venv
+source venv/bin/activate
+pip install -e .
+pip install git+https://github.com/152334H/BigVGAN.git
+pip install streamlit
+python -m streamlit run scripts/app.py
