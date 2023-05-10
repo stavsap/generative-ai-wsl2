3bd408a36a65f70b0c831826eef4175ff5b03b67
Stav Sapir <stavsap@gmail.com>
2023-05-10 22:08:53 +0300

    Create install_full.sh

---
+++ text-gen-webui/install_full.sh
@@ -0,0 +1,11 @@
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt install software-properties-common -y
+sudo add-apt-repository ppa:deadsnakes/ppa -y
+sudo apt install python3.10 python3.10-venv python3-pip git python3.10-tk ffmpeg libsm6 libxext6 libgl1 wget -y
+sudo apt install nvidia-cuda-toolkit -y
+
+git clone https://github.com/oobabooga/one-click-installers.git
+cd one-click-installers
+chmod +x start_linux.sh
+./start_linux.sh
