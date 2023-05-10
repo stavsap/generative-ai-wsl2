6d358ed67313b82ede3311709fec15d9b2d601e4
Stav Sapir <stavsap@gmail.com>
2023-05-10 20:45:00 +0300

    Update install_full.sh

--- kandinsky/install_full.sh
+++ kandinsky/install_full.sh
@@ -5,9 +5,4 @@ sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt install python3.10 python3.10-venv python3-pip git python3.10-tk ffmpeg libsm6 libxext6 libgl1 wget -y
 sudo apt install nvidia-cuda-toolkit -y
 
-git clone https://github.com/seruva19/kubin
-cd kubin
-python3 -m venv venv 
-source venv/bin/activate 
-pip install -r requirements.txt
-python src/kubin.py
+curl -sLS https://raw.githubusercontent.com/stavsap/generative-ai-wsl2/main/kandinsky/install.sh | bash
