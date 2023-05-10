195514eb6416d54d4c14ee2fca36b76cf628aacd
Stav Sapir <stavsap@gmail.com>
2023-05-10 20:49:56 +0300

    Update install.sh

--- kandinsky/install.sh
+++ kandinsky/install.sh
@@ -1,3 +1,9 @@
+curl https://raw.githubusercontent.com/stavsap/generative-ai-wsl2/main/kandinsky/run.sh > run.sh
+chmod +x run.sh
+
+curl https://raw.githubusercontent.com/stavsap/generative-ai-wsl2/main/kandinsky/update.sh > update.sh
+chmod +x update.sh
+
 git clone https://github.com/seruva19/kubin
 cd kubin
 python3 -m venv venv 
