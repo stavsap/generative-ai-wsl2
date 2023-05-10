aeb47e5f92a328e948b784f9b1a98328820d1998
Stav Sapir <stavsap@gmail.com>
2023-05-10 22:39:58 +0300

    Update install.sh

--- text-gen-webui/install.sh
+++ text-gen-webui/install.sh
@@ -1,3 +1,9 @@
+curl https://raw.githubusercontent.com/stavsap/generative-ai-wsl2/main/text-gen-webui/run.sh > run.sh
+chmod +x run.sh
+
+curl https://raw.githubusercontent.com/stavsap/generative-ai-wsl2/main/text-gen-webui/update.sh > update.sh
+chmod +x update.sh
+
 git clone https://github.com/oobabooga/one-click-installers.git
 cd one-click-installers
 chmod +x start_linux.sh
