b49cafc47f66088fa72219450dcadd31169f8ed0
Stav Sapir <stavsap@gmail.com>
2023-05-10 20:31:51 +0300

    Update install_full.sh

--- stable-diffusion/install_full.sh
+++ stable-diffusion/install_full.sh
@@ -5,6 +5,4 @@ sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt install python3.10 python3.10-venv python3-pip git python3.10-tk ffmpeg libsm6 libxext6 libgl1 wget -y
 sudo apt install nvidia-cuda-toolkit -y
 
-git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git
-cd stable-diffusion-webui
-./webui.sh
+curl -sLS https://raw.githubusercontent.com/stavsap/generative-ai-wsl2/main/stable-diffusion/install.sh | bash
