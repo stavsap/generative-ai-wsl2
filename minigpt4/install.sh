f30052c66da499c2143f2234102bbcfe4529ca3f
Stav Sapir <stavsap@gmail.com>
2023-05-11 21:40:48 +0300

    Create install.sh

---
+++ minigpt4/install.sh
@@ -0,0 +1,20 @@
+nvidia-smi
+
+echo "Installing MiniGPT4!"
+
+git clone https://github.com/Vision-CAIR/MiniGPT-4.git
+cd MiniGPT-4/
+
+# if you run 13B , Plaease Download Checkpoint Aligned with Vicuna 13B
+#wget https://huggingface.co/wangrongsheng/MiniGPT4/blob/main/pretrained_minigpt4.pth
+
+# if you run 7B , Plaease Download Checkpoint Aligned with Vicuna 7B
+wget https://huggingface.co/wangrongsheng/MiniGPT4-7B/resolve/main/prerained_minigpt4_7b.pth
+
+sed -i 's,llama_model: \"/path/to/vicuna/weights/\",llama_model: \"wangrongsheng/MiniGPT-4-LLaMA-7B\",' minigpt4/configs/models/minigpt4.yaml
+sed -i 's,/path/to/pretrained/ckpt/,prerained_minigpt4_7b.pth,' eval_configs/minigpt4_eval.yaml
+
+conda env create -f environment.yml
+conda activate minigpt4
+
+python demo.py --cfg-path eval_configs/minigpt4_eval.yaml --gpu-id 0
