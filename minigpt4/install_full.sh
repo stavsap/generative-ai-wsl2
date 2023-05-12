a024c9780033fc5d569433903ed40254b1dc726d
Stav Sapir <stavsap@gmail.com>
2023-05-12 09:09:51 +0300

    Update install_full.sh

--- minigpt4/install_full.sh
+++ minigpt4/install_full.sh
@@ -22,10 +22,31 @@ bash Miniconda3.sh -b
 
 echo 'export CUDA_HOME=/home/$USER/miniconda3/envs/textgen/pkgs/cuda-toolkit/' >> .bashrc
 echo 'export PATH="$PATH:/home/$USER/miniconda3/bin/"' >> .bashrc
-echo 'export PATH="$PATH:/home/$USER/miniconda3/envs/textgen/bin/"' >> .bashrc
 echo 'export LD_LIBRARY_PATH="/usr/lib/wsl/lib:/home/$USER/miniconda3/lib/"' >> .bashrc
 
-/home/$USER/miniconda3/bin/ init bash
+/home/$USER/miniconda3/bin/conda init bash
 
-curl -sLS https://raw.githubusercontent.com/stavsap/generative-ai-wsl2/main/minigpt4/install.sh | bash -i
+nvidia-smi
+
+echo "Installing MiniGPT4!"
+
+git clone https://github.com/Vision-CAIR/MiniGPT-4.git
+cd MiniGPT-4/
+
+# if you run 13B , Plaease Download Checkpoint Aligned with Vicuna 13B
+#wget https://huggingface.co/wangrongsheng/MiniGPT4/blob/main/pretrained_minigpt4.pth
+
+# if you run 7B , Plaease Download Checkpoint Aligned with Vicuna 7B
+wget https://huggingface.co/wangrongsheng/MiniGPT4-7B/resolve/main/prerained_minigpt4_7b.pth
+
+sed -i 's,llama_model: \"/path/to/vicuna/weights/\",llama_model: \"wangrongsheng/MiniGPT-4-LLaMA-7B\",' minigpt4/configs/models/minigpt4.yaml
+sed -i 's,/path/to/pretrained/ckpt/,prerained_minigpt4_7b.pth,' eval_configs/minigpt4_eval.yaml
+
+conda env create -f environment.yml
+source /home/$USER/miniconda3/etc/profile.d/conda.sh # otherwise conda complains about 'shell not initialized' (needed when running in a script)
+conda activate minigpt4
+
+python demo.py --cfg-path eval_configs/minigpt4_eval.yaml --gpu-id 0
+
+
 
