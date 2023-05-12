d132b29166635af79e721e1267e8983f54958efc
Stav Sapir <stavsap@gmail.com>
2023-05-12 08:06:42 +0300

    Update install_base.sh

--- minigpt4/install_base.sh
+++ minigpt4/install_base.sh
@@ -25,4 +25,4 @@ echo 'export PATH="$PATH:/home/$USER/miniconda3/bin/"' >> .bashrc
 echo 'export PATH="$PATH:/home/$USER/miniconda3/envs/textgen/bin/"' >> .bashrc
 echo 'export LD_LIBRARY_PATH="/usr/lib/wsl/lib:/home/$USER/miniconda3/lib/"' >> .bashrc
 
-echo "Refreshing BASH!"
+/home/$USER/miniconda3/bin/conda init bash
