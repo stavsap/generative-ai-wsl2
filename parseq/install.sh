2b491947a71b37edcb1e1a3e7d9be8dd860949c7
Stav Sapir <stavsap@gmail.com>
2023-05-13 18:05:46 +0300

    Update install.sh

--- parseq/install.sh
+++ parseq/install.sh
@@ -1,3 +1,9 @@
+curl https://raw.githubusercontent.com/stavsap/generative-ai-wsl2/main/parseq/run.sh > run.sh
+chmod +x run.sh
+
+curl https://raw.githubusercontent.com/stavsap/generative-ai-wsl2/main/parseq/update.sh > update.sh
+chmod +x update.sh
+
 git clone https://github.com/rewbs/sd-parseq.git
 cd sd-parseq
 npm install
