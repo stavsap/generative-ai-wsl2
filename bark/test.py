9bf120caff465b87e4b8b9e696fe92fe8139b5fe
Stav Sapir <stavsap@gmail.com>
2023-05-15 22:09:03 +0300

    Update test.py

--- bark/test.py
+++ bark/test.py
@@ -17,3 +17,12 @@ write_wav("bark_generation.wav", SAMPLE_RATE, audio_array)
   
 # play text in notebook
 Audio(audio_array, rate=SAMPLE_RATE)
+
+text_prompt = """
+    I have a silky smooth voice, and today I will tell you about 
+    the exercise regimen of the common sloth.
+"""
+audio_array = generate_audio(text_prompt, history_prompt="v2/en_speaker_1")
+
+# save audio to disk
+write_wav("bark_generation2.wav", SAMPLE_RATE, audio_array)
