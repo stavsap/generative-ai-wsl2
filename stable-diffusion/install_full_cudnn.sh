6b7bbc9a1231db620ab6202fdf9618e411da3d1e
Stav Sapir <stavsap@gmail.com>
2023-05-16 21:48:03 +0300

    Update install_full_cudnn.sh

--- stable-diffusion/install_full_cudnn.sh
+++ stable-diffusion/install_full_cudnn.sh
@@ -1,3 +1,9 @@
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt install software-properties-common -y
+sudo add-apt-repository ppa:deadsnakes/ppa -y
+sudo apt install python3.10 python3.10-venv python3-pip git python3.10-tk ffmpeg libsm6 libxext6 libgl1 wget -y
+
 wget https://developer.download.nvidia.com/compute/cuda/repos/wsl-ubuntu/x86_64/cuda-wsl-ubuntu.pin
 sudo mv cuda-wsl-ubuntu.pin /etc/apt/preferences.d/cuda-repository-pin-600
 wget https://developer.download.nvidia.com/compute/cuda/12.1.1/local_installers/cuda-repo-wsl-ubuntu-12-1-local_12.1.1-1_amd64.deb
