a9f6bad86ce353d26cb6ef54fbed5e3b3c52747a
Stav Sapir <stavsap@gmail.com>
2023-05-16 23:13:32 +0300

    Update install.sh

--- privategpt/install.sh
+++ privategpt/install.sh
@@ -7,3 +7,11 @@ mkdir models
 cd models
 wget https://gpt4all.io/models/ggml-gpt4all-j-v1.3-groovy.bin
 wget https://huggingface.co/Pi3141/alpaca-native-7B-ggml/resolve/397e872bf4c83f4c642317a5bf65ce84a105786e/ggml-model-q4_0.bin
+cd ..
+touch privateGPT/.env
+echo "PERSIST_DIRECTORY=db" >> privateGPT/.env
+echo "LLAMA_EMBEDDINGS_MODEL=$PWD/models/ggml-model-q4_0.bin" >> .env
+echo "MODEL_TYPE=GPT4All" >> privateGPT/.env
+echo "MODEL_PATH=$PWD/models/ggml-gpt4all-j-v1.3-groovy.bin"  >> privateGPT/.env
+echo "MODEL_N_CTX=1000" >> privateGPT/.env
+
