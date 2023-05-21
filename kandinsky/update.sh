98c03575b1f552c24660b387b7bbc9bc2fa3db1b
Stav Sapir <stavsap@gmail.com>
2023-05-21 19:40:49 +0300

    Update update.sh

--- kandinsky/update.sh
+++ kandinsky/update.sh
@@ -1,6 +1,24 @@
-cd kubin
-git stash
-git pull
-git stash pop
-cd ..
-echo "All Complete"
+#!/bin/bash
+
+# Custom function to perform git pull, any changes will be stashed and restored.
+perform_git_pull() {
+    # Navigate to the folder
+    cd "$1"
+    
+    # Check if the folder is a Git repository
+    if [ -d ".git" ]; then
+        echo "Performing 'git pull' in: $1"
+        git stash
+        git pull
+        git stash pop
+        echo "--------------------------"
+    else
+        echo "Skipping non-Git folder: $1"
+        echo "--------------------------"
+    fi
+    
+    # Return to the original folder
+    cd - >/dev/null
+}
+
+perform_git_pull "kubin"
