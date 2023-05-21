07aca2ce9e6ddc2ee7741d6a7b191247b65d0758
Stav Sapir <stavsap@gmail.com>
2023-05-21 20:16:27 +0300

    Update install.sh

--- stable-diffusion-vlad-automatic/install.sh
+++ stable-diffusion-vlad-automatic/install.sh
@@ -1,7 +1,7 @@
-curl https://raw.githubusercontent.com/stavsap/generative-ai-wsl2/main/stable-diffusion/run.sh > run.sh
+curl https://raw.githubusercontent.com/stavsap/generative-ai-wsl2/main/stable-diffusion-vlad-automatic/run.sh > run.sh
 chmod +x run.sh
 
-curl https://raw.githubusercontent.com/stavsap/generative-ai-wsl2/main/stable-diffusion/update.sh > update.sh
+curl https://raw.githubusercontent.com/stavsap/generative-ai-wsl2/main/stable-diffusion-vlad-automatic/update.sh > update.sh
 chmod +x update.sh
 
 git clone https://github.com/vladmandic/automatic.git
