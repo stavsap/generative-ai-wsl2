5568730e0fe8d09a332451541ac0bd7e4d115ab3
Stav Sapir <stavsap@gmail.com>
2023-05-25 23:39:58 +0300

    Update install.sh

--- privategpt/install.sh
+++ privategpt/install.sh
@@ -15,4 +15,5 @@ echo "LLAMA_EMBEDDINGS_MODEL=all-MiniLM-L6-v2" >> privateGPT/.env
 echo "MODEL_TYPE=GPT4All" >> privateGPT/.env
 echo "MODEL_PATH=$PWD/models/ggml-gpt4all-j-v1.3-groovy.bin"  >> privateGPT/.env
 echo "MODEL_N_CTX=1000" >> privateGPT/.env
+echo "TARGET_SOURCE_CHUNKS=4" >> privateGPT/.env
 
