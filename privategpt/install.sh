1d98c9600992853e16df01313b50a61dbbdf8f51
Stav Sapir <stavsap@gmail.com>
2023-05-26 20:51:20 +0300

    Update install.sh

--- privategpt/install.sh
+++ privategpt/install.sh
@@ -14,7 +14,7 @@ wget https://huggingface.co/TheBloke/GPT4All-13B-snoozy-GGML/resolve/main/GPT4Al
 cd ..
 touch privateGPT/.env
 echo "PERSIST_DIRECTORY=db" >> privateGPT/.env
-echo "LLAMA_EMBEDDINGS_MODEL=all-MiniLM-L6-v2" >> privateGPT/.env
+echo "EMBEDDINGS_MODEL_NAME=all-MiniLM-L6-v2" >> privateGPT/.env
 echo "MODEL_TYPE=GPT4All" >> privateGPT/.env
 echo "MODEL_PATH=$PWD/models/ggml-gpt4all-j-v1.3-groovy.bin"  >> privateGPT/.env
 echo "MODEL_N_CTX=1000" >> privateGPT/.env
