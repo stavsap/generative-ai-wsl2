bcf5a250ff6dc8d37a8f5eff8cfdb33d1798ef0f
Stav Sapir <stavsap@gmail.com>
2023-05-27 21:41:14 +0300

    Update fix.sh

--- text-gen-webui/fix.sh
+++ text-gen-webui/fix.sh
@@ -1,6 +1,8 @@
+cd one-click-installers
 CONDA_ROOT_PREFIX="$(pwd)/installer_files/conda"
 INSTALL_ENV_DIR="$(pwd)/installer_files/env"
 source "$CONDA_ROOT_PREFIX/etc/profile.d/conda.sh"
 conda activate "$INSTALL_ENV_DIR"
 pip install einops
 pip install xformers
+cd ..
