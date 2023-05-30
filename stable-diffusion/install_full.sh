1bcfc298aad241a2cad5eda09b7fab7de822b536
Stav Sapir <stavsap@gmail.com>
2023-05-30 20:16:01 +0300

    Update install_full.sh

--- stable-diffusion/install_full.sh
+++ stable-diffusion/install_full.sh
@@ -2,7 +2,7 @@ sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt install software-properties-common -y
 sudo add-apt-repository ppa:deadsnakes/ppa -y
-sudo apt install python3.10 python3.10-venv python3-pip git python3.10-tk ffmpeg libsm6 libxext6 libgl1 wget -y
+sudo apt install python3.10 python3.10-venv python3-pip git python3.10-tk ffmpeg libsm6 libxext6 libgl1 wget libcairo2-dev -y
 sudo apt install nvidia-cuda-toolkit -y
 
 curl -sLS https://raw.githubusercontent.com/stavsap/generative-ai-wsl2/main/stable-diffusion/install.sh | bash
