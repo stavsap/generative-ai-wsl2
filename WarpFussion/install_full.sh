57379895a7d3ea9a48636ca9a87d5b7adbff90d4
Stav Sapir <stavsap@gmail.com>
2023-06-11 12:22:47 +0300

    Delete WarpFussion directory

--- WarpFussion/install_full.sh
+++
@@ -1,5 +0,0 @@
-sudo apt-get update && apt-get install -y software-properties-common \
-    && add-apt-repository ppa:deadsnakes/ppa \
-    && apt-get update \
-    && apt-get install -y \ 
-    git python3.10 python3-pip python3-opencv imagemagick ffmpeg
