04d77d6bb0996e5c2b47993048398369ddc6f214
Stav Sapir <stavsap@gmail.com>
2023-06-18 21:49:08 +0300

    Update install_full.sh

--- stable-diffusion/install_full.sh
+++ stable-diffusion/install_full.sh
@@ -3,9 +3,7 @@ sudo apt-get upgrade -y
 sudo apt install software-properties-common -y
 sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt install python3.10 python3.10-venv python3-pip git python3.10-tk ffmpeg libsm6 libxext6 libgl1 wget libcairo2-dev -y
+sudo DEBIAN_FRONTEND=noninteractive apt-get -yq install nvidia-cudnn
 sudo apt install nvidia-cuda-toolkit -y
-export DEBIAN_FRONTEND=noninteractive
-sudo apt install nvidia-cudnn -y
-unset DEBIAN_FRONTEND
 
 curl -sLS https://raw.githubusercontent.com/stavsap/generative-ai-wsl2/main/stable-diffusion/install.sh | bash
