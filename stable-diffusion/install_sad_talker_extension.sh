174f3fbc5a7f2b78f6911520d60d4438c8be9f4c
Stav Sapir <stavsap@gmail.com>
2023-06-20 21:57:19 +0300

    Update install_sad_talker_extension.sh

--- stable-diffusion/install_sad_talker_extension.sh
+++ stable-diffusion/install_sad_talker_extension.sh
@@ -1,6 +1,4 @@
-cd stable-diffusion-webui/extensions
-git clone https://github.com/OpenTalker/SadTalker.git
-cd SadTalker
+cd stable-diffusion-webui/extensions/SadTalker
 bash <(wget -qO- https://raw.githubusercontent.com/Winfredy/SadTalker/main/scripts/download_models.sh)
 cd ../../..
-echo "SadTalker Installed"
+echo "SadTalker Models Installed"
