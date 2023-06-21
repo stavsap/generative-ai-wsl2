4148b8174e08cec34027a67d7021c1562c70b78b
Stav Sapir <stavsap@gmail.com>
2023-06-21 12:20:55 +0300

    Update install_control_net_models.sh

--- stable-diffusion/install_control_net_models.sh
+++ stable-diffusion/install_control_net_models.sh
@@ -12,15 +12,7 @@ mv *.pth /home/$USER/stable-diffusion-webui/models/ControlNet/
 cd ../..
 cd /home/$USER/stable-diffusion-webui/models/ControlNet/
 wget https://huggingface.co/ioclab/ioc-controlnet/resolve/main/models/control_v1p_sd15_brightness.safetensors
-cd /home/$USER/temp
-git clone https://huggingface.co/ioclab/control_v1u_sd15_illumination_webui
-cd control_v1u_sd15_illumination_webui
-mv illumination11000.safetensors /home/$USER/stable-diffusion-webui/models/ControlNet/control_v1u_sd15_illumination11000.safetensors
-mv illumination15000.safetensors /home/$USER/stable-diffusion-webui/models/ControlNet/control_v1u_sd15_illumination15000.safetensors
-mv illumination20000.safetensors /home/$USER/stable-diffusion-webui/models/ControlNet/control_v1u_sd15_illumination20000.safetensors
-cp config.json /home/$USER/stable-diffusion-webui/models/ControlNet/control_v1u_sd15_illumination11000.json
-cp config.json /home/$USER/stable-diffusion-webui/models/ControlNet/control_v1u_sd15_illumination15000.json
-mv config.json /home/$USER/stable-diffusion-webui/models/ControlNet/control_v1u_sd15_illumination20000.json
+wget https://huggingface.co/ioclab/ioc-controlnet/resolve/main/models/control_v1p_sd15_illumination.safetensors
 cd /home/$USER
 rm -rf /home/$USER/temp
 echo "All Control Net Models Downloaded"
