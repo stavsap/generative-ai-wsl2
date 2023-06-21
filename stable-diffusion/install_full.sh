9a0a8651b0ba010fa4127829d466fefb33321642
Stav Sapir <stavsap@gmail.com>
2023-06-21 09:10:38 +0300

    Update install_full.sh

--- stable-diffusion/install_full.sh
+++ stable-diffusion/install_full.sh
@@ -2,7 +2,7 @@ sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt install software-properties-common -y
 sudo add-apt-repository ppa:deadsnakes/ppa -y
-sudo apt install git-lfs python3.10 python3.10-venv python3-pip git python3.10-tk ffmpeg libsm6 libxext6 libgl1 wget libcairo2-dev -y
+sudo apt install unzip git-lfs python3.10 python3.10-venv python3-pip git python3.10-tk ffmpeg libsm6 libxext6 libgl1 wget libcairo2-dev -y
 sudo DEBIAN_FRONTEND=noninteractive apt-get -yq install nvidia-cudnn
 sudo apt install nvidia-cuda-toolkit -y
 
