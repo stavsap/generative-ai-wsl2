3c067d29396aee104cf46c73fca7ad669a9c4cd6
Stav Sapir <stavsap@gmail.com>
2023-06-21 10:49:40 +0300

    Update install_extensions.sh

--- stable-diffusion/install_extensions.sh
+++ stable-diffusion/install_extensions.sh
@@ -1,4 +1,5 @@
 cd /home/$USER/stable-diffusion-webui/extensions
 git clone https://github.com/OpenTalker/SadTalker.git
 curl -sLS https://raw.githubusercontent.com/stavsap/generative-ai-wsl2/main/stable-diffusion/install_sad_talker_extension.sh | bash
-
+git clone https://github.com/Mikubill/sd-webui-controlnet.git
+mkdir /home/$USER/stable-diffusion-webui/models/Controlnet
