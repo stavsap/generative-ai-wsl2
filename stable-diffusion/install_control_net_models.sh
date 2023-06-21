532135921586cbd7bb6172036ac85fe2be1d8f17
Stav Sapir <stavsap@gmail.com>
2023-06-21 11:30:08 +0300

    Update install_control_net_models.sh

--- stable-diffusion/install_control_net_models.sh
+++ stable-diffusion/install_control_net_models.sh
@@ -10,11 +10,9 @@ git clone https://huggingface.co/TencentARC/T2I-Adapter
 cd T2I-Adapter/models
 mv *.pth /home/$USER/stable-diffusion-webui/models/ControlNet/
 cd ../..
-git clone https://huggingface.co/ioclab/control_v1p_sd15_brightness
-cd control_v1p_sd15_brightness
-mv diffusion_pytorch_model.safetensors /home/$USER/stable-diffusion-webui/models/ControlNet/control_v1p_sd15_brightness.safetensors
-mv config.json /home/$USER/stable-diffusion-webui/models/ControlNet/control_v1p_sd15_brightness.json
-cd ..
+cd /home/$USER/stable-diffusion-webui/models/ControlNet/
+wget https://huggingface.co/ioclab/ioc-controlnet/resolve/main/models/control_v1p_sd15_brightness.safetensors
+cd /home/$USER/temp
 git clone https://huggingface.co/ioclab/control_v1u_sd15_illumination_webui
 cd control_v1u_sd15_illumination_webui
 mv illumination11000.safetensors /home/$USER/stable-diffusion-webui/models/ControlNet/control_v1u_sd15_illumination11000.safetensors
