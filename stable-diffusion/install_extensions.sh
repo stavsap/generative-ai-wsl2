6d9922ec0b76e3506ca4f49704c8a15297298089
Stav Sapir <stavsap@gmail.com>
2023-06-21 08:54:31 +0300

    Update install_extensions.sh

--- stable-diffusion/install_extensions.sh
+++ stable-diffusion/install_extensions.sh
@@ -1,3 +1,4 @@
 cd /home/$USER/stable-diffusion-webui/extensions
 git clone https://github.com/OpenTalker/SadTalker.git
+curl -sLS https://raw.githubusercontent.com/stavsap/generative-ai-wsl2/main/stable-diffusion/install_sad_talker_extension.sh | bash
 
