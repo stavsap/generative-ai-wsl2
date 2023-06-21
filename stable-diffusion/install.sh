71c764c1fbe5067739d1432f94cda722d6f06e9d
Stav Sapir <stavsap@gmail.com>
2023-06-21 09:12:11 +0300

    Update install.sh

--- stable-diffusion/install.sh
+++ stable-diffusion/install.sh
@@ -5,6 +5,10 @@ curl https://raw.githubusercontent.com/stavsap/generative-ai-wsl2/main/stable-di
 chmod +x update.sh
 
 git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git
+cd stable-diffusion-webui
+python -m venv venv
+source venv/bin/activate
+python -m pip install xformers
 curl -sLS https://raw.githubusercontent.com/stavsap/generative-ai-wsl2/main/stable-diffusion/install_extensions.sh | bash
 cd /home/$USER
 ./run.sh
