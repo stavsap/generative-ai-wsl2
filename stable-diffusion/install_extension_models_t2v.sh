e129d0b0dec79d9e904a1499572f31084beb675b
Stav Sapir <stavsap@gmail.com>
2023-06-24 19:48:23 +0300

    Update install_extension_models_t2v.sh

--- stable-diffusion/install_extension_models_t2v.sh
+++ stable-diffusion/install_extension_models_t2v.sh
@@ -1,8 +1,5 @@
-mkdir /home/$USER/stable-diffusion-webui/models/ModelScope/t2v
+mkdir /home/$USER/stable-diffusion-webui/models/ModelScope
 cd /home/$USER/stable-diffusion-webui/models/ModelScope
 git lfs install
 git clone https://huggingface.co/damo-vilab/modelscope-damo-text-to-video-synthesis
-cd modelscope-damo-text-to-video-synthesis 
-mv * ../t2v/
-cd ..
-rm -rf modelscope-damo-text-to-video-synthesis
+mv modelscope-damo-text-to-video-synthesis/ t2v/
