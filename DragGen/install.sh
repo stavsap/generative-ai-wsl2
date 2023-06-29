95725ecad1c99e93e313818be27e2147d89ffa90
Stav Sapir <stavsap@gmail.com>
2023-06-29 20:53:56 +0300

    testing

--- DragGen/install.sh
+++ DragGen/install.sh
@@ -2,6 +2,6 @@ git clone https://github.com/XingangPan/DragGAN.git
 cd DragGAN
 conda env create -f environment.yml
 conda activate stylegan3
-pip install -r requirements
+pip install -r requirements.txt
 python scripts/download_model.py
 sh scripts/gui.sh
