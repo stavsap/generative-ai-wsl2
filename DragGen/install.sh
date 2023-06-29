81abbd3c2b30f1bb36605dac13bc720c18c9bc5f
Stav Sapir <stavsap@gmail.com>
2023-06-29 22:49:34 +0300

    Update install.sh

--- DragGen/install.sh
+++ DragGen/install.sh
@@ -4,10 +4,12 @@ echo "========================="
 
 git clone https://github.com/XingangPan/DragGAN.git
 cd DragGAN
-conda env create -f environment.yml
-conda activate stylegan3
+sudo apt install gcc-10 g++-10
+export CC=/usr/bin/gcc-10
+export CXX=/usr/bin/g++-10
+export CUDA_ROOT=/usr/local/cuda
 python -m pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
-python -m pip install click numpy imgui glfw pyopengl gradio
+python -m pip install click numpy imgui glfw pyopengl gradio ninja
 pip install -r requirements.txt
 python scripts/download_model.py
 sh scripts/gui.sh
