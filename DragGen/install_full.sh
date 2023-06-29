8ec9c707da4a11d92a63a29df7006427d9f42e30
Stav Sapir <stavsap@gmail.com>
2023-06-29 20:28:11 +0300

    Update install_full.sh

--- DragGen/install_full.sh
+++ DragGen/install_full.sh
@@ -1,1 +1,9 @@
-todo
+
+
+
+echo "========================="
+echo "Installing Miniconda3!"
+echo "========================="
+
+curl -sL "https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh" > "Miniconda3.sh"
+bash Miniconda3.sh -b 
