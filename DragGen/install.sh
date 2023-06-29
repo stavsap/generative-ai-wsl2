27827302b0188126acecc4244eb7236c96c70aac
Stav Sapir <stavsap@gmail.com>
2023-06-29 22:12:53 +0300

    Update install.sh

--- DragGen/install.sh
+++ DragGen/install.sh
@@ -7,7 +7,7 @@ cd DragGAN
 conda env create -f environment.yml
 conda activate stylegan3
 python -m pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
-python -m pip install click numpy
+python -m pip install click numpy imgui glfw
 pip install -r requirements.txt
 python scripts/download_model.py
 sh scripts/gui.sh
