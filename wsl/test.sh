f6d677f566c6a1506f6c1e44399c9bd343ec3ee6
Stav Sapir <stavsap@gmail.com>
2023-07-03 19:54:44 +0300

    Update test.sh

--- wsl/test.sh
+++ wsl/test.sh
@@ -9,12 +9,12 @@ echo 'export CUDA_HOME=/home/$USER/miniconda3/envs/textgen/pkgs/cuda-toolkit/' >
 echo 'export PATH="$PATH:/home/$USER/miniconda3/bin/"' >> .bashrc
 echo 'export LD_LIBRARY_PATH="/usr/lib/wsl/lib:/home/$USER/miniconda3/lib/"' >> .bashrc
 
-/home/$USER/miniconda3/bin/ init bash
+/home/$USER/miniconda3/bin/init bash
 
 source .bashrc
 nvidia-smi
 
-conda create -n textgen python=3.10.9
+conda create -n textgen python=3.10.9 -y
 conda activate textgen
 pip install bitsandbytes
 
