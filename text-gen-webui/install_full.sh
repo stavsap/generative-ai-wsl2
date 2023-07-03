e798940b93de5973976ae799012a6b940ec8e689
Stav Sapir <stavsap@gmail.com>
2023-07-03 20:08:47 +0300

    Update install_full.sh

--- text-gen-webui/install_full.sh
+++ text-gen-webui/install_full.sh
@@ -5,4 +5,4 @@ sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt install python3.10 python3.10-venv python3-pip git python3.10-tk ffmpeg libsm6 libxext6 libgl1 wget -y
 sudo apt install nvidia-cuda-toolkit -y
 
-curl -sLS https://raw.githubusercontent.com/stavsap/generative-ai-wsl2/main/text-gen-webui/install.sh | bash
+curl -sLS https://raw.githubusercontent.com/stavsap/generative-ai-wsl2/main/text-gen-webui/install_conda.sh | bash
