103b1542215d6b26ee3c6af8789e56359fc7115d
Stav Sapir <stavsap@gmail.com>
2023-07-03 19:59:52 +0300

    Update test.sh

--- wsl/test.sh
+++ wsl/test.sh
@@ -1,7 +1,3 @@
-echo "========================="
-echo "Installing Miniconda3!"
-echo "========================="
-
 curl -sL "https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh" > "Miniconda3.sh"
 bash Miniconda3.sh -b 
 
@@ -9,8 +5,8 @@ echo 'export CUDA_HOME=/home/$USER/miniconda3/envs/textgen/pkgs/cuda-toolkit/' >
 echo 'export PATH="$PATH:/home/$USER/miniconda3/bin/"' >> .bashrc
 echo 'export LD_LIBRARY_PATH="/usr/lib/wsl/lib:/home/$USER/miniconda3/lib/"' >> .bashrc
 
-/home/$USER/miniconda3/bin/ init bash
-
+source .bashrc
+conda init bash
 source .bashrc
 nvidia-smi
 
