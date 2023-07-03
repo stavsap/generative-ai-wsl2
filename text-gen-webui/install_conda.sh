c81c82bc96fc5c4603109605c8bd68970b553378
Stav Sapir <stavsap@gmail.com>
2023-07-03 20:08:33 +0300

    Create install_conda.sh

---
+++ text-gen-webui/install_conda.sh
@@ -0,0 +1,20 @@
+curl -sL "https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh" > "Miniconda3.sh"
+bash Miniconda3.sh -b 
+
+echo 'export CUDA_HOME=/home/$USER/miniconda3/envs/textgen/pkgs/cuda-toolkit/' >> .bashrc
+echo 'export PATH="$PATH:/home/$USER/miniconda3/bin/"' >> .bashrc
+echo 'export LD_LIBRARY_PATH="/usr/lib/wsl/lib:/home/$USER/miniconda3/lib/"' >> .bashrc
+
+source .bashrc
+conda init bash
+source .bashrc
+nvidia-smi
+
+conda create -n textgen python=3.10.9 -y
+conda activate textgen
+git clone https://github.com/oobabooga/text-generation-webui
+cd text-generation-webui
+pip install -r requirements.txt
+pip install bitsandbytes
+conda install cudatoolkit
+python server.py
