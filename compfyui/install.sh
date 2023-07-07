91baacc13661904c1e9d061d6337fe013f92401a
Stav Sapir <stavsap@gmail.com>
2023-07-07 09:50:22 +0300

    Update install.sh

--- compfyui/install.sh
+++ compfyui/install.sh
@@ -1,1 +1,6 @@
-
+git clone https://github.com/comfyanonymous/ComfyUI.git
+cd ComfyUI
+python3 -m venv venv
+source venv/bin/activate
+python -m pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu118 xformer
+python -m pip install -r requirements.txt
