efe0ca799f71a3233f30e9e4a06d8b7a3582bcb6
Stav Sapir <stavsap@gmail.com>
2023-07-09 23:08:14 +0300

    Delete run-wsl.bat

--- wsl/run-wsl.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-wsl --list
-
-echo ----------------------------------------------
-set /p disto=Select wsl instance to run:
-set /p user_name=- Enter user name:
-start wsl -d %disto% -u %user_name% --cd /home/%user_name%
-PAUSE
