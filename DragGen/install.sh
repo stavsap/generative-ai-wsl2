0c5f631cf71df39df3f1fee757bde54977b93cdd
Stav Sapir <stavsap@gmail.com>
2023-07-09 21:06:13 +0300

    Update install.sh

--- DragGen/install.sh
+++ DragGen/install.sh
@@ -2,10 +2,10 @@ echo "========================="
 echo "Installing DragGAN!"
 echo "========================="
 
-curl https://raw.githubusercontent.com/stavsap/generative-ai-wsl2/main/stable-diffusion/run.sh > run.sh
+curl https://raw.githubusercontent.com/stavsap/generative-ai-wsl2/main/DragGen/run.sh > run.sh
 chmod +x run.sh
 
-curl https://raw.githubusercontent.com/stavsap/generative-ai-wsl2/main/stable-diffusion/update.sh > update.sh
+curl https://raw.githubusercontent.com/stavsap/generative-ai-wsl2/main/DragGen/update.sh > update.sh
 chmod +x update.sh
 
 git clone https://github.com/XingangPan/DragGAN.git
