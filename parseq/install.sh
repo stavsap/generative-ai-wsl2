47f1540f50b96872b81489b630934a4db906abbd
Stav Sapir <stavsap@gmail.com>
2023-07-10 22:01:59 +0300

    Update install.sh

--- parseq/install.sh
+++ parseq/install.sh
@@ -6,5 +6,8 @@ chmod +x update.sh
 
 git clone https://github.com/rewbs/sd-parseq.git
 cd sd-parseq
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+source ~/.bashrc
+nvm install node
 npm install
 npm start
