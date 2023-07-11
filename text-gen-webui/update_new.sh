c2dc95e450cd257cdb2ae02982949964bd350ed1
Stav Sapir <stavsap@gmail.com>
2023-07-11 21:41:52 +0300

    Create update_new.sh

---
+++ text-gen-webui/update_new.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Specify the extenstions folder path
+folder_path="extensions"
+
+# Custom function to perform git pull, any changes will be stashed and restored.
+install_extension() {
+    # Navigate to the folder
+    cd "$1"
+    
+    # Check if the folder is a Git repository
+    python -m pip install -r requierments.txt
+    
+    # Return to the original folder
+    cd - >/dev/null
+}
+
+for folder in "$folder_path"/*; do
+    # Check if the current item is a directory
+    if [ -d "$folder" ]; then
+        
+        install_extension "$folder"
+    fi
+done
