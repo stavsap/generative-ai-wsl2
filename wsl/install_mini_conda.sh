6b5bb6c830fbe09817504a63f683adf32b388d15
Stav Sapir <stavsap@gmail.com>
2023-07-14 21:57:17 +0300

    Update install_mini_conda.sh

--- wsl/install_mini_conda.sh
+++ wsl/install_mini_conda.sh
@@ -1,9 +1,3 @@
-echo ""
-echo "========================="
-echo "Installing Miniconda3!"
-echo "========================="
-echo ""
-
 curl -sL "https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh" > "Miniconda3.sh"
 bash Miniconda3.sh -b 
 
