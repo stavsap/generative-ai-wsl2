98b1dc9061677bfdd041c0f1198a1b3fa837c2e8
Stav Sapir <stavsap@gmail.com>
2023-07-14 13:35:28 +0300

    Update install_autotrain.sh

--- llm-training/install_autotrain.sh
+++ llm-training/install_autotrain.sh
@@ -8,16 +8,16 @@ conda install cudatoolkit -y
 conda install pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia -y
 conda install -c conda-forge hdbscan -y
 
-pip install -U bitsandbytes -y
-pip install -U git+https://github.com/huggingface/transformers.git -y
-pip install -U git+https://github.com/huggingface/peft.git -y
-pip install -U git+https://github.com/huggingface/accelerate.git -y
-pip install datasets -y
-pip install scipy -y
+pip install -U bitsandbytes
+pip install -U git+https://github.com/huggingface/transformers.git
+pip install -U git+https://github.com/huggingface/peft.git
+pip install -U git+https://github.com/huggingface/accelerate.git
+pip install datasets
+pip install scipy
 
-pip install psycopg2 -y
-pip install psycopg2-binary -y
-pip install sentencepiece -y
-pip install tiktoken -y
+pip install psycopg2
+pip install psycopg2-binary
+pip install sentencepiece
+pip install tiktoken
 
 pip install autotrain-advanced
