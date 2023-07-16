e0c6e4998a45743eb0c85fadfe15ea62f4891c55
Stav Sapir <stavsap@gmail.com>
2023-07-16 21:18:04 +0300

    Update soft_links.sh

--- compfyui/soft_links.sh
+++ compfyui/soft_links.sh
@@ -1,4 +1,4 @@
 # soft links to models, embeddings etc.
 rm -rf /home/stav/ComfyUI/models/checkpoints
 
-ln -s /mnt/g/WSL/SharedAssets/sd-checkpoints /home/stav/ComfyUI/models/checkpoints
+ln -s /mnt/g/WSL/SharedAssets/sd-checkpoints /home/$USER/ComfyUI/models/checkpoints
