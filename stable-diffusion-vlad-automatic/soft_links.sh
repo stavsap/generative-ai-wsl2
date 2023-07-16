b4a39d1215197d2573e407f5b09e9c3c91d66a83
Stav Sapir <stavsap@gmail.com>
2023-07-16 18:46:59 +0300

    Update soft_links.sh

--- stable-diffusion-vlad-automatic/soft_links.sh
+++ stable-diffusion-vlad-automatic/soft_links.sh
@@ -1,2 +1,3 @@
 # soft links to models, embeddings etc.
 ln -s /mnt/g/WSL/SharedAssets/sd-checkpoints /home/$USER/automatic/models/Stable-diffusion
+ln -s /mnt/g/Generative-AI/SharedOutputs/StableDiffusionVlad /home/$USER/automatic/outputs
