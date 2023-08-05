f6e695bad0b812fd9a84083593c0f9020c52abcf
Stav Sapir <stavsap@gmail.com>
2023-08-05 20:28:46 +0300

    Update install_full.sh

--- StableSwarmUI/install_full.sh
+++ StableSwarmUI/install_full.sh
@@ -1,3 +1,5 @@
 curl -sLS https://raw.githubusercontent.com/stavsap/generative-ai-wsl2/main/wsl/cuda_cudnn_base.sh | bash
+sudo apt-get install -y dotnet-sdk-7.0
 sudo apt-get install -y aspnetcore-runtime-7.0
+sudo apt-get install -y dotnet-runtime-7.0
 curl -sLS https://raw.githubusercontent.com/stavsap/generative-ai-wsl2/main/StableSwarmUI/install.sh | bash
