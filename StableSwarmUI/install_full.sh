6d01b4e7078597315a990e8d4b0a2eca9c6581e9
Stav Sapir <stavsap@gmail.com>
2023-08-05 20:24:10 +0300

    Update install_full.sh

--- StableSwarmUI/install_full.sh
+++ StableSwarmUI/install_full.sh
@@ -1,1 +1,3 @@
-f
+curl -sLS https://raw.githubusercontent.com/stavsap/generative-ai-wsl2/main/wsl/cuda_cudnn_base.sh | bash
+
+curl -sLS https://raw.githubusercontent.com/stavsap/generative-ai-wsl2/main/stable-diffusion-vlad-automatic/install.sh | bash
