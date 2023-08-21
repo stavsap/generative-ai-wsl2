d887e047daa33c992d7b8b86c920c8f214b07ad9
Stav Sapir <stavsap@gmail.com>
2023-08-21 22:18:52 +0300

    Update install_full.sh

--- fooocus/install_full.sh
+++ fooocus/install_full.sh
@@ -1,1 +1,2 @@
 curl -sLS https://raw.githubusercontent.com/stavsap/generative-ai-wsl2/main/wsl/cuda_cudnn_base.sh | bash
+curl -sLS https://raw.githubusercontent.com/stavsap/generative-ai-wsl2/main/wsl/install_mini_conda.sh | bash
