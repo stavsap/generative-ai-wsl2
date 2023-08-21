501a9afb8fad1478660d6d151841299c35cdcf19
Stav Sapir <stavsap@gmail.com>
2023-08-21 22:19:56 +0300

    Update install_full.sh

--- fooocus/install_full.sh
+++ fooocus/install_full.sh
@@ -1,2 +1,8 @@
 curl -sLS https://raw.githubusercontent.com/stavsap/generative-ai-wsl2/main/wsl/cuda_cudnn_base.sh | bash
 curl -sLS https://raw.githubusercontent.com/stavsap/generative-ai-wsl2/main/wsl/install_mini_conda.sh | bash
+
+git clone https://github.com/lllyasviel/Fooocus.git
+cd Fooocus
+conda env create -f environment.yaml
+conda activate fooocus
+pip install -r requirements_versions.txt
