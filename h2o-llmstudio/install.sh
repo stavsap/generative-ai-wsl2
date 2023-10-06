1337abdf633ea79ad41f2bfd1bc203ff5dc584d8
Stav Sapir <stavsap@gmail.com>
2023-10-06 14:15:33 +0300

    Delete h2o-llmstudio directory

--- h2o-llmstudio/install.sh
+++
@@ -1,9 +0,0 @@
-curl https://raw.githubusercontent.com/stavsap/generative-ai-wsl2/main/h2o-llmstudio/run.sh > run.sh
-chmod +x run.sh
-
-curl https://raw.githubusercontent.com/stavsap/generative-ai-wsl2/main/h2o-llmstudio/update.sh > update.sh
-chmod +x update.sh
-
-git clone https://github.com/h2oai/h2o-llmstudio.git
-cd h2o-llmstudio
-make setup
