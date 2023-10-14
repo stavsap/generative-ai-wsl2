986f023fe11e814eb6d121252ff50410d638e45e
Stav Sapir <stavsap@gmail.com>
2023-10-14 12:31:06 +0300

    Create install_full.sh

---
+++ LLaVA/install_full.sh
@@ -0,0 +1,2 @@
+curl -sLS https://raw.githubusercontent.com/stavsap/generative-ai-wsl2/main/wsl/cuda_cudnn_base.sh | bash
+curl -sLS https://raw.githubusercontent.com/stavsap/generative-ai-wsl2/main/LLaVA/install.sh | bash
