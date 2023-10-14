19984f973a7a96c3e579d57f199b9674e88a10ef
Stav Sapir <stavsap@gmail.com>
2023-10-14 13:34:52 +0300

    Update install_full.sh

--- LLaVA/install_full.sh
+++ LLaVA/install_full.sh
@@ -1,2 +1,11 @@
-curl -sLS https://raw.githubusercontent.com/stavsap/generative-ai-wsl2/main/wsl/cuda_cudnn_miniconda.sh | bash -i
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt install software-properties-common -y
+sudo add-apt-repository ppa:deadsnakes/ppa -y
+sudo apt install unzip git-lfs python3.10 python3.10-venv python3-pip git python3.10-tk ffmpeg libsm6 libxext6 libgl1 wget libcairo2-dev -y
+sudo DEBIAN_FRONTEND=noninteractive apt-get -yq install nvidia-cudnn
+sudo apt install nvidia-cuda-toolkit -y
+nvidia-smi
+
+
 curl -sLS https://raw.githubusercontent.com/stavsap/generative-ai-wsl2/main/LLaVA/install.sh | bash -i
