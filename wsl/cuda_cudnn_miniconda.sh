22ade0dcbf61e0ce246dcaed62dfaf2371dbeb0e
Stav Sapir <stavsap@gmail.com>
2023-10-14 15:16:38 +0300

    Update cuda_cudnn_miniconda.sh

--- wsl/cuda_cudnn_miniconda.sh
+++ wsl/cuda_cudnn_miniconda.sh
@@ -1,31 +1,11 @@
-echo "========================="
-echo "Installing Cuda!"
-echo "========================="
-
-wget https://developer.download.nvidia.com/compute/cuda/repos/wsl-ubuntu/x86_64/cuda-wsl-ubuntu.pin
-sudo mv cuda-wsl-ubuntu.pin /etc/apt/preferences.d/cuda-repository-pin-600
-wget https://developer.download.nvidia.com/compute/cuda/12.1.1/local_installers/cuda-repo-wsl-ubuntu-12-1-local_12.1.1-1_amd64.deb
-sudo dpkg -i cuda-repo-wsl-ubuntu-12-1-local_12.1.1-1_amd64.deb
-sudo cp /var/cuda-repo-wsl-ubuntu-12-1-local/cuda-*-keyring.gpg /usr/share/keyrings/
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install cuda -y
-sudo apt install build-essential -y
-
-echo "========================="
-echo "Installing Cudnn!"
-echo "========================="
-
-sudo wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin 
-sudo mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
-sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/3bf863cc.pub
-sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/ /" -y
-sudo apt-get update
-sudo apt-get install libcudnn8=8.9.1.*-1+cuda12.1 libcudnn8-dev=8.9.1.*-1+cuda12.1 libcudnn8-samples=8.9.1.*-1+cuda12.1 -y
-
-echo "========================="
-echo "Installing Miniconda3!"
-echo "========================="
+sudo apt install software-properties-common -y
+sudo add-apt-repository ppa:deadsnakes/ppa -y
+sudo apt install unzip git-lfs python3.10 python3.10-venv python3-pip git python3.10-tk ffmpeg libsm6 libxext6 libgl1 wget libcairo2-dev -y
+sudo DEBIAN_FRONTEND=noninteractive apt-get -yq install nvidia-cudnn
+sudo apt install nvidia-cuda-toolkit -y
+nvidia-smi
 
 curl -sL "https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh" > "Miniconda3.sh"
 bash Miniconda3.sh -b 
@@ -34,4 +14,5 @@ echo 'export CUDA_HOME=/home/$USER/miniconda3/envs/textgen/pkgs/cuda-toolkit/' >
 echo 'export PATH="$PATH:/home/$USER/miniconda3/bin/"' >> .bashrc
 echo 'export LD_LIBRARY_PATH="/usr/lib/wsl/lib:/home/$USER/miniconda3/lib/"' >> .bashrc
 
-/home/$USER/miniconda3/bin/ init bash
+source .bashrc
+/home/$USER/miniconda3/bin/conda init bash
