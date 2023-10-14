3c49093ac70f8ea64e5ec4f833ce344929549ae0
Stav Sapir <stavsap@gmail.com>
2023-10-14 13:35:54 +0300

    Update install_full.sh

--- LLaVA/install_full.sh
+++ LLaVA/install_full.sh
@@ -7,5 +7,14 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get -yq install nvidia-cudnn
 sudo apt install nvidia-cuda-toolkit -y
 nvidia-smi
 
+curl -sL "https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh" > "Miniconda3.sh"
+bash Miniconda3.sh -b 
+
+echo 'export CUDA_HOME=/home/$USER/miniconda3/envs/textgen/pkgs/cuda-toolkit/' >> .bashrc
+echo 'export PATH="$PATH:/home/$USER/miniconda3/bin/"' >> .bashrc
+echo 'export LD_LIBRARY_PATH="/usr/lib/wsl/lib:/home/$USER/miniconda3/lib/"' >> .bashrc
+
+source .bashrc
+/home/$USER/miniconda3/bin/ init bash
 
 curl -sLS https://raw.githubusercontent.com/stavsap/generative-ai-wsl2/main/LLaVA/install.sh | bash -i
