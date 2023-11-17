cac0b2bfb8b2d8397c45c821fa45a62c51303ae3
Stav Sapir <stavsap@gmail.com>
2023-11-17 13:19:21 +0200

    Update install_full.sh

--- comfyui/install_full.sh
+++ comfyui/install_full.sh
@@ -1,2 +1,2 @@
 curl -sLS https://raw.githubusercontent.com/stavsap/generative-ai-wsl2/main/wsl/cuda_cudnn_base.sh | bash
-curl -sSL https://raw.githubusercontent.com/stavsap/generative-ai-wsl2/main/compfyui/install.sh | bash
+curl -sSL https://raw.githubusercontent.com/stavsap/generative-ai-wsl2/main/comfyui/install.sh | bash
