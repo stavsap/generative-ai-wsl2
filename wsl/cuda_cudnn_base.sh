ac320d3afa49bd290c4120f3e726b46512088848
Stav Sapir <stavsap@gmail.com>
2024-03-18 23:21:33 +0200

    Update cuda_cudnn_base.sh

--- wsl/cuda_cudnn_base.sh
+++ wsl/cuda_cudnn_base.sh
@@ -3,6 +3,13 @@ sudo apt-get upgrade -y
 sudo apt install software-properties-common -y
 sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt install unzip git-lfs python3.10 python3.10-venv python3-pip git python3.10-tk ffmpeg libsm6 libxext6 libgl1 wget libcairo2-dev -y
-sudo DEBIAN_FRONTEND=noninteractive apt-get -yq install nvidia-cudnn
-sudo apt install nvidia-cuda-toolkit -y
+
+wget https://developer.download.nvidia.com/compute/cuda/repos/wsl-ubuntu/x86_64/cuda-wsl-ubuntu.pin
+sudo mv cuda-wsl-ubuntu.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget https://developer.download.nvidia.com/compute/cuda/12.4.0/local_installers/cuda-repo-wsl-ubuntu-12-4-local_12.4.0-1_amd64.deb
+sudo dpkg -i cuda-repo-wsl-ubuntu-12-4-local_12.4.0-1_amd64.deb
+sudo cp /var/cuda-repo-wsl-ubuntu-12-4-local/cuda-*-keyring.gpg /usr/share/keyrings/
+sudo apt-get update
+sudo apt-get -y install cuda-toolkit-12-4
+
 nvidia-smi
